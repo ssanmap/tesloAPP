f6bb9b193df384f1e8cb83d5d4cbc578b873cb8f
ssanmartinp <ssanmartinp@falabella.cl>
2023-09-27 15:56:47 -0300

    first teslo app

---
+++ README.md
@@ -0,0 +1,17 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Teslo API
+
+1. Clonar Proyecto
+2. yarn install 
+3. clonar el archivo .env.template y renombrarlo a .env
+4. cambiar las variables de entorno
+
+5. Levantar la bbdd
+```
+docker-compose up -d
+```
+
+6. yarn start:dev 
---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { config } from 'process';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    // import other modules here
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+     
+    }),
+    ProductsModule,
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
